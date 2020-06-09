616d553c9c586ec89bbaf52eb179844e94034d7f
StefPY <stefan.mail.sk@gmail.com>
2020-06-09 07:53:40 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# qr_reader-writer
-Light code to generate QR code as well as read
+# QR code generator & reader
+
+Light code to generate QR code as well as read.
+
+<b> READING</b>
+
+```
+rd = Read()
+rd.read_QR_code(path)
+```
+
+In order read the QR code your webcam will start up.
+When you show the QR code, it will be registered in the "register.txt" file in format "time***QR-data " i.e.:
+
+``` Tue Jun  9 07:28:24 2020***https://barcodesegypt.com ```
+
+To QUIT use "q"
+
+<b> GENERATING </b>
+
+To generate QR-code use:
+```
+rd = Read()
+rd.getFromImage('Test qr code', True)
+```
+```True``` if you want to open the generated QR code.
+
+The QR code will be saved as "QRcode...jpg"
+
+
---
+++ main.py
@@ -0,0 +1,56 @@
+import qrcode
+from PIL import Image
+import os
+import cv2
+from pyzbar import pyzbar
+import time
+
+class Read:
+    def getFromImage(self, text, show=False):
+        """
+        PilImage formate QRcode
+        """
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4,)
+        qr.add_data(text)
+        qr.make(fit=True)
+        raw_image = qr.make_image(fill_color='black', back_color="white")
+        raw_image.save(f'QRcode{time.time()}.jpg')
+
+        if show:
+            raw_image.show()
+        else:
+            return raw_image
+
+    def read_QR_code(self, path_output_dir):
+        report_history = []
+        report = []
+        vidcap = cv2.VideoCapture(0)
+        succ, img = vidcap.read()
+        succ = True
+        count = 0
+        while succ:
+            cv2.imshow('img', img)
+            succ, img = vidcap.read()
+            cv2.imwrite(os.path.join(path_output_dir, 'qrcode.png'), img)
+            count += 1
+            data = pyzbar.decode(Image.open(os.path.join(path_output_dir, 'qrcode.png')))
+            os.remove(os.path.join(path_output_dir, 'qrcode.png'))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            if data:
+                for info in data:
+                    actTime = time.asctime()
+                    if info.data.decode() not in report_history:
+                        report_history.append(info.data.decode())
+                        if report_history[-1] != report:
+                            report = report_history[-1]
+                            with open('register.txt', 'a') as f:
+                                f.writelines(actTime + "***" + report + "\n" )
+                                print(report)
+        cv2.destroyAllWindows()
+        vidcap.release()
+
+
+rd = Read()
+rd.getFromImage('Test qr code', True)
+# rd.read_QR_code('C:\\Users\\stefan.gal\\Documents\\Python\\Projects\\')
